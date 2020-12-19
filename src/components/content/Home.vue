dbc349f1d11fc98dac2ce5cec2baea90ba99434c
wang <wxy1376258881@163.com>
2020-12-19 16:13:41 +0800

    添加空文件

---
+++ src/components/content/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="lists">
+    <Item v-for="(v, i) in items" :key="i" :item="v"></Item>
+  </div>
+</template>
+
+<script>
+import Item from "./Item";
+export default {
+  name: "",
+  components: { Item },
+  data() {
+    return {};
+  },
+  props: ["items"],
+};
+</script>
+
+<style></style>
